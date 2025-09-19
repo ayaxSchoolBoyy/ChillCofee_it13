0cdb84201670c757fed02c83807f045b04eccd80
ayaxSchoolBoyy <k.limos.143675.tc@umindanao.edu.ph>
2025-09-19 19:38:59 +0800

    Add files via upload
    sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE users
+(
+	id INT PRIMARY KEY IDENTITY(1,10),
+	username VARCHAR(MAX) NULL,
+	password VARCHAR(MAX) NULL,
+	profile_image VARCHAR(MAX) NULL,
+	role VARCHAR(MAX) NULL,
+	status VARCHAR(MAX) NULL,
+	date_rag DATE NULL
+)
+
+SELECT * FROM users
+
